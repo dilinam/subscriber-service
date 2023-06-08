0993159685ab79043ee81c000e98b14188fdb18b
Dilina Madhushan <dilinamadhushan.and@gmail.com>
2023-06-08 22:00:46 +0530

    error fixed

--- src/main/java/org/dtf202/subscriberservice/service/RevenueScheduler.java
+++ src/main/java/org/dtf202/subscriberservice/service/RevenueScheduler.java
@@ -21,7 +21,7 @@ public class RevenueScheduler {
     private final UserRepository userRepository;
     private final UserPackageRepository userPackageRepository;
 
-    @Scheduled(cron = "0 0 0 0 0 MON-FRI")
+    @Scheduled(cron = "0 1 * * * *")
     public void revScheduler(){
         LocalDateTime dt = LocalDateTime.now();
         Optional<List<UserPackage>> userPkList= userPackageRepository.findAllByStatusIsTrue();
